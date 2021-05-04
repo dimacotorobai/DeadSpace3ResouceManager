4ff80da7342ad1cee62b27289a3605991b8c85fc
dimacotorobai <52585921+dimacotorobai@users.noreply.github.com>
2021-05-04 14:28:46 -0400

    Update

--- DeadSpace3ResouceManager/main.cpp
+++ DeadSpace3ResouceManager/main.cpp
@@ -16,6 +16,7 @@ struct DS3
 	std::vector<int> SomaticGel_Offset = { 0xDD3B8C, 0x8, 0xE4, 0x74, 0x8, 0x98, 0x80, 0xE00 };
 	std::vector<int> Transducer_Offset = { 0xDD3B8C, 0x8, 0xE4, 0x74, 0x8, 0x98, 0x80, 0xE04 };
 	std::vector<int> RationSeals_Offset = { 0xDD3B8C, 0x8, 0xE4, 0x74, 0x8, 0x98, 0x80, 0xE08 };
+
 	DS3(HANDLE hProcess, uintptr_t uiModuleBaseAddress)
 	{
 		Tungsten = FindDynamicAddress(hProcess, uiModuleBaseAddress, Tungsten_Offset);
@@ -26,7 +27,7 @@ struct DS3
 		RationSeals = FindDynamicAddress(hProcess, uiModuleBaseAddress, RationSeals_Offset);
 	}
 
-	//Delete Copy constructor and assignment operator
+	//Delete Copy constructor and assignment operator(Not necessary to do)
 	DS3(const DS3&) = delete;
 	DS3& operator=(const DS3&) = delete;
 };
@@ -41,8 +42,8 @@ int main(int argc, char **argv)
 		uintptr_t uiBA = GetModuleBaseAddress(L"deadspace3.exe", dwPID);
 		if (uiBA)
 		{
-			std::cout << std::hex << "PID: " << dwPID << std::endl;
-			std::cout << "Base Address: " << uiBA << std::dec << std::endl;
+			std::cout << std::hex << "PID: 0x" << dwPID << std::endl;
+			std::cout << "Base Address: 0x" << uiBA << std::dec << std::endl;
 			HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, dwPID);
 			if (hProcess != INVALID_HANDLE_VALUE)
 			{
@@ -53,11 +54,11 @@ int main(int argc, char **argv)
 
 				//Main Loop
 				std::cout << "Press [F1] for Tungsten" << std::endl;
-				std::cout << "Press [F2] for Tungsten" << std::endl;
-				std::cout << "Press [F3] for Tungsten" << std::endl;
-				std::cout << "Press [F4] for Tungsten" << std::endl;
-				std::cout << "Press [F5] for Tungsten" << std::endl;
-				std::cout << "Press [F6] for Tungsten" << std::endl;
+				std::cout << "Press [F2] for Semicoductor" << std::endl;
+				std::cout << "Press [F3] for Scrap Metal" << std::endl;
+				std::cout << "Press [F4] for Somatic Gel" << std::endl;
+				std::cout << "Press [F5] for Transducer" << std::endl;
+				std::cout << "Press [F6] for Ration Seals" << std::endl;
 				std::cout << "Press [F7] to exit" << std::endl;
 
 				uintptr_t buffer;
@@ -73,30 +74,41 @@ int main(int argc, char **argv)
 					}
 					if (GetAsyncKeyState(VK_F2))
 					{
-
+						ReadProcessMemory(hProcess, (LPCVOID)addresses.Semiconductor, &buffer, sizeof(buffer), 0);
+						buffer += 500;
+						WriteProcessMemory(hProcess, (LPVOID)addresses.Semiconductor, &buffer, sizeof(buffer), 0);
 					}
 					if (GetAsyncKeyState(VK_F3))
 					{
-
+						ReadProcessMemory(hProcess, (LPCVOID)addresses.ScrapMetal, &buffer, sizeof(buffer), 0);
+						buffer += 500;
+						WriteProcessMemory(hProcess, (LPVOID)addresses.ScrapMetal, &buffer, sizeof(buffer), 0);
 					}
 					if (GetAsyncKeyState(VK_F4))
 					{
-
+						ReadProcessMemory(hProcess, (LPCVOID)addresses.SomaticGel, &buffer, sizeof(buffer), 0);
+						buffer += 500;
+						WriteProcessMemory(hProcess, (LPVOID)addresses.SomaticGel, &buffer, sizeof(buffer), 0);
 					}
 					if (GetAsyncKeyState(VK_F5))
 					{
-
+						ReadProcessMemory(hProcess, (LPCVOID)addresses.Transducer, &buffer, sizeof(buffer), 0);
+						buffer += 500;
+						WriteProcessMemory(hProcess, (LPVOID)addresses.Transducer, &buffer, sizeof(buffer), 0);
 					}
 					if (GetAsyncKeyState(VK_F6))
 					{
-
+						ReadProcessMemory(hProcess, (LPCVOID)addresses.RationSeals, &buffer, sizeof(buffer), 0);
+						buffer += 500;
+						WriteProcessMemory(hProcess, (LPVOID)addresses.RationSeals, &buffer, sizeof(buffer), 0);
 					}
 					if (GetAsyncKeyState(VK_F7))
 					{
 						break;
 					}
-
-
+					
+					//Sleep for 200ms to slow thread and to not spam the keys;
+					Sleep(200);
 				}
 
 			}
@@ -104,7 +116,5 @@ int main(int argc, char **argv)
 		}
 
 	}
-
-	std::cin.get();
 	return 0;
 }
